3b6fa5221168c07c9df5d3c279c16a388d1ce061
Thunder0408 <166932263+Thunder0408@users.noreply.github.com>
2024-04-14 23:50:25 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -305,10 +305,12 @@ with col[0]:
     st.altair_chart(chart3_1, use_container_width=True)
     st.altair_chart(chart3_2, use_container_width=True)
     st.altair_chart(chart3_3, use_container_width=True)
+    heatmap = make_heatmap(grouped_10, 'คณะที่คุณกำลังศึกษา', 'จำนวนนักศึกษา', 'Blues')
+    st.altair_chart(heatmap, use_container_width=True)
+    
 with col[1]:
     st.altair_chart(chart6, use_container_width=True)
     st.altair_chart(chart8, use_container_width=True)
     st.altair_chart(chart9, use_container_width=True)
-    heatmap = make_heatmap(grouped_10, 'คณะที่คุณกำลังศึกษา', 'จำนวนนักศึกษา', 'Blues')
-    st.altair_chart(heatmap, use_container_width=True)
+    
 
