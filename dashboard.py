437dc04c718d62d18c68714b2a2b4daf0a8380c7
Thunder0408 <166932263+Thunder0408@users.noreply.github.com>
2024-04-15 13:54:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -278,33 +278,6 @@ chart9 = alt.Chart(df).mark_bar().encode(
     title='ผลของสถานที่พักต่อการใช้จ่ายของนักศึกษา'
 )
 
-# Assuming df is your original DataFrame and it has been properly loaded
-grouped_10 = df.groupby(["คณะที่คุณกำลังศึกษา"]).size().reset_index(name="จำนวนนักศึกษา")
-
-def make_heatmap(input_df, input_y, input_color, input_color_theme):
-    # Calculate the number of unique faculties to adjust the height dynamically
-    num_faculties = input_df[input_y].nunique()
-    chart_height = num_faculties * 20  # Adjust multiplier to scale height as needed
-
-    heatmap = alt.Chart(input_df).mark_rect().encode(
-        y=alt.Y(f'{input_y}:O', axis=alt.Axis(title=input_y, titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-        color=alt.Color(f'max({input_color}):Q',
-                        legend=alt.Legend(title="Number of Students"),
-                        scale=alt.Scale(scheme=input_color_theme)),
-        stroke=alt.value('black'),
-        strokeWidth=alt.value(0.25),
-    ).properties(
-        width=400,  # Adjust width as needed
-        height=chart_height  # Dynamically set height based on number of faculties
-    ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=12
-    )
-    return heatmap
-
-# Create the heatmap
-heatmap = make_heatmap(grouped_10, 'คณะที่คุณกำลังศึกษา', 'จำนวนนักศึกษา', 'Blues')
-
 with col[0]:
     st.altair_chart(chart2, use_container_width=True)
     st.altair_chart(chart3_1, use_container_width=True)
@@ -315,5 +288,4 @@ with col[1]:
     st.altair_chart(chart6, use_container_width=True)
     st.altair_chart(chart8, use_container_width=True)
     st.altair_chart(chart9, use_container_width=True)
-    st.altair_chart(heatmap, use_container_width=True)
 
